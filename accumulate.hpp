8613f2b0baf7f4c9ada373f67ef3f05560e21118
yiratpeleg <yiratpeleg8@gmail.com>
2020-06-08 21:21:37 +0300

    start range

---
+++ accumulate.hpp
@@ -0,0 +1,14 @@
+//
+// Created by yirat on 08/06/2020.
+//
+#pragma once
+
+namespace itertools{
+    template <typename T> class accumulate{
+        T iterable;
+    public:
+        accumulate(T it): iterable(it){}
+
+
+    };
+}
---
+++ compress.hpp
@@ -0,0 +1,4 @@
+//
+// Created by yirat on 08/06/2020.
+//
+#pragma once
